3bfd2352beb75e19ea83b21c8bc3d173dcf7ee3c
OWamsley <owamsley10@gmail.com>
2022-08-21 16:41:44 -0600

    Party Entry page functionality nearly done.

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,23 +1,54 @@
 import React from 'react';
 import Header from '../Components/Header/index';
 import Counter from '../Components/Counter/index';
+import PartyEntry from '../Components/PartyEntry/index';
 
 class HomePage extends React.Component{
     constructor(props){
         super(props);
         this.incPartyCount = this.incPartyCount.bind(this);
         this.decPartyCount = this.decPartyCount.bind(this);
+        this.updateParty = this.updateParty.bind(this);
+        this.updateParties = this.updateParties.bind(this);
         this.state = {
-            partyCount: 4
+            partyCount: 4,
+            parties: [{
+                Number: 1,
+                Name: "joe"
+            }]
         };
     }
     render(){
+        const parties = this.state.parties.map((party)=> <>{party.Number}{party.Name}</>)
         return(
         <>
             <Header/>
+            <div>
+            current parties: 
+            {parties}
+            </div>
             <Counter partyCount={this.state.partyCount} decreaseCount={this.decPartyCount} increaseCount={this.incPartyCount} />
-
+            <PartyEntry updateParties={this.updateParties} partyCount={this.state.partyCount} updateParty={this.state.updateParty} />
         </>)
+    }
+
+    
+
+    updateParty(number, name){
+        //Number needs to be index of party array i.e. the first name entry labeled 1 on
+        //screen needs to have number 0.
+        let updateparties = [... this.state.parties];
+        updateparties[number].Name = name;
+        updateparties[number].Number = number;
+        this.setState({
+            parties: updateparties
+        });
+    }
+
+    updateParties(updateParties){
+        this.setState(
+            {parties: updateParties}
+        );
     }
 
     incPartyCount(){
